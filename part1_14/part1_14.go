56fb40b1bcd4d315cf126c9c42219fe1f467a74c
Ashley Winter <ashley.winter@bjss.com>
2024-08-14 22:46:44 +0100

    part 14 - now observing race conditions

--- part1_14/part1_14.go
+++ part1_14/part1_14.go
@@ -1,25 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-var numbers []int32
+var number int
 
-func updateData(even bool) {
-	if even {
-		numbers = append(numbers, numbers[len(numbers)-1]+2)
-	} else {
-		numbers = append(numbers, numbers[len(numbers)-1]+1)
+var waitGroup = new(sync.WaitGroup)
+
+func updateNumberOdds() {
+	defer waitGroup.Done()
+	for i := 1; i < 150; i += 2 {
+		number = i
+		fmt.Println("odds", number)
 	}
+
+}
+
+func updateNumberEvens() {
+	defer waitGroup.Done()
+	for i := 2; i < 150; i += 2 {
+		number = i
+		fmt.Println("evens", number)
+	}
+
 }
 
 func main() {
 
-	numbers = append(numbers, 1)
-
-	for _ = range 50 {
-		go updateData(false)
-		go updateData(true)
-
-		fmt.Println(numbers)
-	}
+	waitGroup.Add(2)
+	go updateNumberOdds()
+	go updateNumberEvens()
+	waitGroup.Wait()
 }
