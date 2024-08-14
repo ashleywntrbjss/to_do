989a4037e1e637767eb4e710bddd386504ee6a27
Ashley Winter <ashley.winter@bjss.com>
2024-08-14 08:40:54 +0100

    simplify part 11

--- part1_11/part1_11.go
+++ part1_11/part1_11.go
@@ -10,10 +10,19 @@ type ToDoItem struct{
 	Title string `json:"title"`
 }
 
-func ToDoItemsAsJson(arg ...ToDoItem){
-	toDoItems := []ToDoItem{}
-
-	toDoItems = append(toDoItems, arg...)
+func ToDoItemsAsJson(){
+	item1 := ToDoItem{1, "Washing up"}
+	item2 := ToDoItem{2, "Ironing"}
+	item3 := ToDoItem{3, "Food shop"}
+	item4 := ToDoItem{4, "Wash car"}
+	item5 := ToDoItem{5, "Take out recycling"}
+	item6 := ToDoItem{6, "Dust the mantle"}
+	item7 := ToDoItem{7, "Whites wash"}
+	item8 := ToDoItem{8, "Lights wash"}
+	item9 := ToDoItem{9, "Dark wash"}
+	item10 := ToDoItem{10, "Return parcel"}
+	
+	toDoItems := []ToDoItem{item1, item2, item3, item4, item5, item6, item7, item8, item9, item10}
 	marshalledItems, _ := json.Marshal(toDoItems)
 	fmt.Println(string(marshalledItems))
 }
--- part1_11/part1_11_test.go
+++ part1_11/part1_11_test.go
@@ -1,6 +1,6 @@
 package part1_11
 
 func ExampleToDoItemsAsJson(){
-	ToDoItemsAsJson(ToDoItem{1, "Washing up"}, ToDoItem{2, "Ironing"}, ToDoItem{3, "Food shop"}, ToDoItem{4,  "Wash car"})
-	// Output: [{"id":1,"title":"Washing up"},{"id":2,"title":"Ironing"},{"id":3,"title":"Food shop"},{"id":4,"title":"Wash car"}]
+	ToDoItemsAsJson()
+	// Output: [{"id":1,"title":"Washing up"},{"id":2,"title":"Ironing"},{"id":3,"title":"Food shop"},{"id":4,"title":"Wash car"},{"id":5,"title":"Take out recycling"},{"id":6,"title":"Dust the mantle"},{"id":7,"title":"Whites wash"},{"id":8,"title":"Lights wash"},{"id":9,"title":"Dark wash"},{"id":10,"title":"Return parcel"}]
 }
