a1692de88373cbc2c8280b886b411745df585b4a
Ashley Winter <ashley.winter@bjss.com>
2024-08-14 16:47:45 +0100

    writes to file

--- part1_12/part1_12.go
+++ part1_12/part1_12.go
@@ -1,19 +1,37 @@
-package part1_12
+package main
 
 import (
-    "fmt"
-    "encoding/json"
+	"encoding/json"
+	"log"
+	"os"
 )
 
-type ToDoItem struct{
-	Id int `json:"id"`
+type ToDoItem struct {
+	Id    int    `json:"id"`
 	Title string `json:"title"`
 }
 
-func ToDoItemsAsJson(arg ...ToDoItem){
+func marshallToDoItems(args ...ToDoItem) []byte {
 	toDoItems := []ToDoItem{}
 
-	toDoItems = append(toDoItems, arg...)
+	toDoItems = append(toDoItems, args...)
 	marshalledItems, _ := json.Marshal(toDoItems)
-	fmt.Println(string(marshalledItems))
+	return marshalledItems
+}
+
+func bytesToFile(encodedData []byte) {
+
+	err := os.WriteFile("file.txt", encodedData, 0666)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	toDoItems := []ToDoItem{{1, "Washing up"}, {2, "Ironing"}, {3, "Food shop"}, {4, "Wash car"}, {5, "Buy fish and chips"}, {6, "Renew car insurance"}, {7, "Complete Go tasks"}, {8, "return parcel"}, {9, "Organise cutlery drawer"}, {10, "Dust the mantle"}}
+
+	marshalledToDoItems := marshallToDoItems(toDoItems...)
+
+	bytesToFile(marshalledToDoItems)
 }
--- part1_12/part1_12_test.go
+++ part1_12/part1_12_test.go
@@ -1,6 +1,6 @@
-package part1_12
+package main
 
 func ExampleToDoItemsAsJson() {
-	ToDoItemsAsJson(ToDoItem{1, "Washing up"}, ToDoItem{2, "Ironing"}, ToDoItem{3, "Food shop"}, ToDoItem{4, "Wash car"}, ToDoItem{5, "Buy fish and chips"}, ToDoItem{6, "Renew car insurance"}, ToDoItem{7, "Complete Go tasks"}, ToDoItem{8, "return parcel"}, ToDoItem{9, "Organise cutlery drawer"}, ToDoItem{10, "Dust the mantle"})
+	marshallToDoItems(ToDoItem{1, "Washing up"}, ToDoItem{2, "Ironing"}, ToDoItem{3, "Food shop"}, ToDoItem{4, "Wash car"}, ToDoItem{5, "Buy fish and chips"}, ToDoItem{6, "Renew car insurance"}, ToDoItem{7, "Complete Go tasks"}, ToDoItem{8, "return parcel"}, ToDoItem{9, "Organise cutlery drawer"}, ToDoItem{10, "Dust the mantle"})
 	// Output: [{"id":1,"title":"Washing up"},{"id":2,"title":"Ironing"},{"id":3,"title":"Food shop"},{"id":4,"title":"Wash car"},{"id":5,"title":"Buy fish and chips"},{"id":6,"title":"Renew car insurance"},{"id":7,"title":"Complete Go tasks"},{"id":8,"title":"return parcel"},{"id":9,"title":"Organise cutlery drawer"},{"id":10,"title":"Dust the mantle"}]
 }
