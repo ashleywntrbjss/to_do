7a24d647026a0c437d95855b177c2cf6eaa2cfe1
Ashley Winter <ashley.winter@bjss.com>
2024-08-16 15:47:16 +0100

    Home and view working

--- part2_18_todo_app_web/main.go
+++ part2_18_todo_app_web/main.go
@@ -5,12 +5,16 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", handleHomePage)
+
+	mux.HandleFunc("/view-all", handleViewAllPage)
 
 	fmt.Println("Starting server at http://localhost:8080")
 	err := http.ListenAndServe("localhost:8080", mux)
@@ -22,13 +26,37 @@ func main() {
 func handleHomePage(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("GET", "Root '/' ")
 
-	activeTemplate, err := template.ParseFiles("home.gohtml")
+	templatePath := filepath.Join("part2_18_todo_app_web", "home.gohtml")
+
+	activeTemplate, err := template.ParseFiles(templatePath)
 	if err != nil {
+		cwd, err := os.Getwd()
 		fmt.Println("Error parsing template:", err)
+		fmt.Println("Current working directory:", cwd)
 		return
 	}
 
 	err = activeTemplate.Execute(writer, nil)
+	if err != nil {
+		fmt.Println("Error executing template:", err)
+		return
+	}
+}
+
+func handleViewAllPage(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("GET", "'/view-all'")
+
+	templatePath := filepath.Join("part2_18_todo_app_web", "viewAll.gohtml")
+
+	activeTemplate, err := template.ParseFiles(templatePath)
+	if err != nil {
+		cwd, err := os.Getwd()
+		fmt.Println("Error parsing template:", err)
+		fmt.Println("Current working directory:", cwd)
+		return
+	}
+
+	err = activeTemplate.Execute(writer, GetAll())
 	if err != nil {
 		fmt.Println("Error executing template:", err)
 		return
