24b082fad0f45b686e49e08e6ec9a3781ce98abe
Ashley Winter <ashley.winter@bjss.com>
2024-08-16 08:38:33 +0100

    Add files

---
+++ part2_todo_app/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func menu() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("================================")
+	fmt.Println("Welcome to the To Do Application")
+	fmt.Println("================================")
+
+	fmt.Println("Please select an option: ")
+}
+
+func Main() {
+
+}
---
+++ part2_todo_app/todoitem.go
@@ -0,0 +1,6 @@
+package main
+
+type ToDoItem struct {
+	Title      string `json:"title"`
+	IsComplete bool   `json:"is_complete"`
+}
