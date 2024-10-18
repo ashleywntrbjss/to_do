32cf29dd7f1de01637e067bf53eff38f4b24398b
Ashley Winter <ashley.winter@bjss.com>
2024-10-18 13:59:26 +0100

    interrupt to close

--- part2_todo_app/web/main.go
+++ part2_todo_app/web/main.go
@@ -3,9 +3,29 @@ package main
 import (
 	"bjss.com/ashley.winter/to_do/part2_todo_app/web/api"
 	"bjss.com/ashley.winter/to_do/part2_todo_app/web/ssr"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	go ssr.ListenAndServe()
 	go api.ListenAndServe()
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	doneChan := make(chan bool, 1)
+
+	go func() {
+		<-signalChan
+		fmt.Println("\nReceived an interrupt, performing cleanup...")
+		// Perform any cleanup here
+		doneChan <- true
+	}()
+
+	fmt.Println("Press Ctrl+C to exit")
+	<-doneChan
+	fmt.Println("Cleanup complete, exiting.")
 }
