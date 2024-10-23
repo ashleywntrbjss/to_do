cab1e5a38f06d1b4d424574f7a4057df965ec5ee
Ashley Winter <ashley.winter@bjss.com>
2024-10-23 11:28:12 +0100

    Add printing, error handling

--- part2_todo_app/repo/sql/postgres.go
+++ part2_todo_app/repo/sql/postgres.go
@@ -3,6 +3,7 @@ package sql
 import (
 	"bjss.com/ashley.winter/to_do/part2_todo_app/todoitem"
 	"errors"
+	"fmt"
 	"github.com/go-pg/pg/v10"
 	"github.com/go-pg/pg/v10/orm"
 )
@@ -40,52 +41,69 @@ func createSchema(db *pg.DB) error {
 
 func (r *PostgresStore) CreateItemFromTitle(title string) (todoitem.ToDoItem, error) {
 	newItem := todoitem.NewToDoItem(title)
-	_, err := r.db.Model(newItem).Insert()
+	result, err := r.db.Model(&newItem).Insert()
 	if err != nil {
 		return todoitem.ToDoItem{}, err
 	}
+	fmt.Println("Added item, rows affected:", result.RowsAffected())
 	return newItem, nil
 }
 
 func (r *PostgresStore) AddNew(item todoitem.ToDoItem) (int, error) {
-	_, err := r.db.Model(item).Insert()
+	result, err := r.db.Model(&item).Insert()
 	if err != nil {
 		return -1, err
 	}
+	fmt.Println("Added item, rows affected:", result.RowsAffected())
 	return item.Id, nil
 }
 func (r *PostgresStore) GetById(itemId int) (todoitem.ToDoItem, error) {
 	var item todoitem.ToDoItem
-	err := r.db.Model(item).Where("id = ?", itemId).Select()
+	err := r.db.Model(&item).Where("id = ?", itemId).Select()
 	if err != nil {
-		if err == pg.ErrNoRows {
+		if errors.Is(err, pg.ErrNoRows) {
 			return todoitem.ToDoItem{}, errors.New("cannot find item by provided id")
 		}
 		return todoitem.ToDoItem{}, err
 	}
+	fmt.Println("Found item by id:", item)
 	return item, nil
 }
 
 func (r *PostgresStore) GetAll() ([]todoitem.ToDoItem, error) {
 	var items []todoitem.ToDoItem
-	err := r.db.Model(items).Select()
+	err := r.db.Model(&items).Select()
 	if err != nil {
 		return nil, err
 	}
+	fmt.Println("Finding all items")
 	return items, nil
 }
 
 func (r *PostgresStore) UpdateItemTitleById(newTitle string, itemId int) error {
-	_, err := r.db.Model(todoitem.ToDoItem{}).Set("title = ?", newTitle).Where("id = ?", itemId).Update()
-	return err
+	result, err := r.db.Model(&todoitem.ToDoItem{}).Set("title = ?", newTitle).Where("id = ?", itemId).Update()
+	if err != nil {
+		return err
+	}
+	fmt.Println("Updating item title, rows affected:", result.RowsAffected())
+	return nil
 }
 
 func (r *PostgresStore) UpdateItemCompletionStatusById(completionStatus bool, itemId int) error {
-	_, err := r.db.Model(todoitem.ToDoItem{}).Set("is_complete = ?", completionStatus).Where("id = ?", itemId).Update()
+	result, err := r.db.Model(&todoitem.ToDoItem{}).Set("is_complete = ?", completionStatus).Where("id = ?", itemId).Update()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Updating item completion status, rows affected:", result.RowsAffected())
 	return err
 }
 
 func (r *PostgresStore) DeleteItemById(itemId int) error {
-	_, err := r.db.Model(&todoitem.ToDoItem{}).Where("id = ?", itemId).Delete()
+	result, err := r.db.Model(&todoitem.ToDoItem{}).Where("id = ?", itemId).Delete()
+	if err != nil {
+		return err
+	}
+	fmt.Println("Deleting item, rows affected:", result.RowsAffected())
 	return err
 }
